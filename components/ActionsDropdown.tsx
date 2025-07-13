ab730113386e99f8dc37df09f8df160c7cf18959
Ariel Berardi <ariel.berardi95@gmail.com>
2025-07-13 16:59:23 +0100

    Implement rename file action

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -21,11 +21,102 @@ import Image from "next/image";
 import Link from "next/link";
 import { Models } from "node-appwrite";
 import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { renameFiles } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 const ActionsDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDropwdownOpen, setIsDropdownOpen] = useState(false);
   const [action, setAction] = useState<ActionType | null>(null);
+  const [name, setName] = useState(file.name);
+  const [emails, setEmails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const path = usePathname();
+
+  const closeAllModals = () => {
+    setIsModalOpen(false);
+    setIsDropdownOpen(false);
+    setAction(null);
+    setName(file.Name);
+  };
+
+  const handleAction = async () => {
+    if (!action) return;
+
+    setIsLoading(true);
+
+    const actions = {
+      rename: () => renameFiles({ fileId: file.$id, name, extension: file.extension, path }),
+      share: () => {},
+      delete: () => {},
+    };
+
+    const success = await actions[action.value as keyof typeof actions]();
+    if (success) closeAllModals();
+
+    setIsLoading(false);
+  };
+
+  // const handleRemoveUser = async (email: string) => {
+  //   const updatedEmails = emails.filter((e) => e !== email);
+
+  //   const success = await updateFileUsers({
+  //     fileId: file.$id,
+  //     emails: updatedEmails,
+  //     path,
+  //   });
+
+  //   if (success) setEmails(updatedEmails);
+  //   closeAllModals();
+  // };
+
+  const renderDialogContent = () => {
+    if (!action) return null;
+
+    const { value, label } = action;
+
+    return (
+      <DialogContent className="shad-dialog button">
+        <DialogHeader className="flex flex-col gap-3">
+          <DialogTitle className="text-center text-light-100">{label}</DialogTitle>
+          {value === "rename" && (
+            <Input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          )}
+          {/* {value === "details" && <FileDetails file={file} />}
+          {value === "share" && (
+            <ShareInput file={file} onInputChange={setEmails} onRemove={handleRemoveUser} />
+          )}
+          {value === "delete" && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete{` `}
+              <span className="delete-file-name">{file.name}</span>?
+            </p>
+          )} */}
+        </DialogHeader>
+        {["rename", "delete", "share"].includes(value) && (
+          <DialogFooter className="flex flex-col gap-3 md:flex-row">
+            <Button onClick={closeAllModals} className="modal-cancel-button">
+              Cancel
+            </Button>
+            <Button onClick={handleAction} className="modal-submit-button">
+              <p className="capitalize">{value}</p>
+              {isLoading && (
+                <Image
+                  src="/assets/icons/loader.svg"
+                  alt="loader"
+                  width={24}
+                  height={24}
+                  className="animate-spin"
+                />
+              )}
+            </Button>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    );
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
@@ -67,6 +158,7 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
           ))}
         </DropdownMenuContent>
       </DropdownMenu>
+      {renderDialogContent()}
     </Dialog>
   );
 };
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -87,3 +87,23 @@ export const getFiles = async () => {
     handleError(error, "Failed to get files");
   }
 };
+
+export const renameFiles = async ({ fileId, name, extension, path }: RenameFileProps) => {
+  const { databases } = await createAdminClient();
+  try {
+    const newName = `${name}.${extension}`;
+    const updatedFile = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      fileId,
+      {
+        name: newName,
+      }
+    );
+
+    revalidatePath(path);
+    return parseStringify(updatedFile);
+  } catch (error) {
+    handleError(error, "Failed to rename file");
+  }
+};
